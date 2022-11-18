7eb6d5a90787312deee88a07a16ab0c000def55f
Pikimir22 <egor.nikitin22@yandex.ru>
2022-11-18 13:46:02 +0300

    v/1

---
+++ src/test/java/tests/AuthTests.java
@@ -0,0 +1,73 @@
+package src.test.java.tests;
+
+
+import config.ConfigurationCenter;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.AuthPage;
+import pages.MainPage;
+import pages.PrivateOfficePage;
+
+
+
+
+    @Tag("auth")
+    public class AuthTests extends TestBase {
+        Faker faker = new Faker();
+        AuthPage authPage = new AuthPage();
+        MainPage mainPage = new MainPage();
+        PrivateOfficePage privateOfficePage = new PrivateOfficePage();
+
+        private final String errorMessage = "Введен неверный номер телефона или пароль",
+                negativeLogin = "7903" + faker.numerify("#######"),
+                negativePassword = "7903" + faker.numerify("#######"),
+                accountStatusMessage = "Ваш аккаунт заблокирован администратором сервиса";
+
+        @Test
+        @AllureId("11102")
+        @Severity(SeverityLevel.BLOCKER)
+        @DisplayName("Авторизация пользователя со статусом Активен")
+        void authActiveUser() {
+            mainPage.openPage()
+                    .clickPrivateOfficeButton();
+            authPage.setLogin(ConfigurationCenter.configTestData.userActiveLogin())
+                    .setPassword(ConfigurationCenter.configTestData.userActivePassword())
+                    .submitClick();
+            privateOfficePage
+                    .clickProfileButton()
+                    .checkProfileTable(ConfigurationCenter.configTestData.userActiveFirstName(), ConfigurationCenter.configTestData.userActiveLastName(),
+                            ConfigurationCenter.configTestData.expectPhoneActiveUser());
+        }
+
+        @Test
+        @AllureId("11108")
+        @Severity(SeverityLevel.NORMAL)
+        @DisplayName("Негативный тест авторизации")
+        void negativeAuthTest() {
+            mainPage.openPage()
+                    .clickPrivateOfficeButton();
+            authPage.setLogin(negativeLogin)
+                    .setPassword(negativePassword)
+                    .submitClick()
+                    .checkErrorMessage(errorMessage);
+        }
+
+        @Test
+        @AllureId("11103")
+        @DisplayName("Авторизация заблокированного пользователя")
+        void authBlockUser() {
+            mainPage.openPage()
+                    .clickPrivateOfficeButton();
+            authPage.setLogin(ConfigurationCenter.configTestData.userBlockLogin())
+                    .setPassword(ConfigurationCenter.configTestData.userBlockPassword())
+                    .submitClick();
+            privateOfficePage.checkAccountStatus(accountStatusMessage)
+                    .checkProfileTable(ConfigurationCenter.configTestData.userBlockFirstName(), ConfigurationCenter.configTestData.userBlockLastName(),
+                            ConfigurationCenter.configTestData.userBlockLogin());
+        }
+    }
+
---
+++ src/test/java/tests/LandingPageTests.java
@@ -0,0 +1,112 @@
+package src.test.java.tests;
+
+import helpers.DriverUtils;
+import helpers.DataBaseQuery;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LandingPageTests extends TestBase {
+    Faker faker = new Faker();
+    DataBaseQuery bd = new DataBaseQuery();
+    private final String unregisteredLogin = "7903" + faker.numerify("#######");
+    private final String unregisteredPassword = "7903" + faker.numerify("#######");
+    private final String city = System.getProperty("city", "Камень-на-Оби");
+    private final String price = bd.findPrice(city).split("\\.")[0];
+
+    @Test
+    @AllureId("11105")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Проверка стоимости на главной странице")
+    void priceCityTests() {
+        step("Открыть главную страницу", () ->
+                open(""));
+
+        step("Выбрать город", () -> {
+            $("div.city-select.selected-city", 1).click();
+            $("#find-city").setValue(city);
+            $("li.ui-menu-item").click();
+        });
+
+        step("Проверить стоимость уборки для выбранного города", () -> {
+            $("div.city-select.selected-city", 1).shouldHave(text(city));
+            $("div.form-cost__value > span.form-cost__sum").shouldHave(text(price));
+        });
+    }
+
+    @Test
+    @AllureId("11111")
+    @DisplayName("Проверка страницы 'Сотрудничество'")
+    void forPerformersTest() {
+        step("Открыть главную страницу", () ->
+                open(""));
+
+        step("Нажать кнопку 'Исполнителям'", () ->
+                $(By.linkText("Исполнителям")).click());
+
+        step("Проверка контента страницы 'Сотрудничество'", () -> {
+            $("div.header-desc.heading-performers").shouldHave(text("Сотрудничество с нами!"));
+            $("h2.employment__heading.heading-performers").shouldHave(text("Регистрация в сервисе"));
+        });
+    }
+
+    @Test
+    @AllureId("11109")
+    @DisplayName("Негативный тест авторизации")
+    void negativeAuthTest() {
+        step("Открыть главную страницу", () ->
+                open(""));
+
+        step("Перейти на страницу авторизации", () ->
+                $("a.menu-item_login", 1).click());
+
+        step("Ввести несуществующую пару логин/пароль", () -> {
+            $("input[name=phone]").setValue(unregisteredLogin);
+            $("input[name=password]").setValue(unregisteredPassword);
+        });
+
+        step("Нажать кнопку 'Войти'", () ->
+                $(".button.button-main").$("span").click());
+
+        step("Проверка подсказки о неверной паре логин/пароль", () ->
+                $("div.form__notice.form__notice_error:nth-child(4)").shouldHave(
+                        text("Введен неверный номер телефона или пароль")));
+    }
+
+    @Test
+    @AllureId("11107")
+    @DisplayName("Проверка соответствия в Title")
+    void titleTest() {
+        step("Открыть главную страницу", () ->
+                open(""));
+
+        step("Title соответствует требованию текста 'Live In Clean'", () -> {
+            String expectedTitle = "Live In Clean";
+            String actualTitle = title();
+            assertThat(actualTitle).isEqualTo(expectedTitle);
+        });
+    }
+
+    @Test
+    @AllureId("11110")
+    @DisplayName("Проверка ошибок в консоли")
+    void consoleShouldNotHaveErrorsTest() {
+        step("Открыть главную страницу", () ->
+                open(""));
+
+        step("Проверка консоли на наличие ошибки 'SEVERE'", () -> {
+            String consoleLogs = DriverUtils.getConsoleLogs();
+            String errorText = "SEVERE";
+            assertThat(consoleLogs).doesNotContain(errorText);
+        });
+    }
+}
